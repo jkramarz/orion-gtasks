b01ee747cf9a2af8fdb6354fde219b33c45dfab1
jkramarz <lenwe@lenwe.net>
2012-10-28 09:10:57 -0400

    License info added

--- javascripts/gtasksPlugin.js
+++ javascripts/gtasksPlugin.js
@@ -1,4 +1,14 @@
-/*global window orion */
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2012 Jakub Kramarz
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: Jakub Kramarz
+ ******************************************************************************/
+
 window.onload = function() {
 	function showGtasks(){
 		var outline_div = parent.document.getElementById('outline');
